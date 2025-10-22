1e8512c927ba1fcf0178d3c4bd6aecb035292189
OopVenster <aidenrenwick@gmail.com>
2025-10-22 22:50:06 +0200

    V.1.3.1 Test Git hub
    This is a test for my main code. This test Included Changed API with changed stat card, code does not include my filter , card is not currently displaying info on card, I need to use v.1.2.3 for stat card to display movie information. So var V.1.2.3 might be better for the group work, i will not continue with this version if the team does not liked this mixed code. 
    
    compared to old code:
    filter is on V.1.2.3 
    Working card on V.1.2.3 
    New API V.1.3.1

--- js/script.js
+++ js/script.js
@@ -1,45 +1,223 @@
-class Movie{
-    constructor(image, release_date, Title, original_language,overview, rating, link){
-    this.image = image;
-    this.release_date = release_date;
-    this.Title = Title;
-    this.original_language = original_language;
-    this.overview = overview;
-    this.rating = rating;
-    this.link = link;
+class Movies {
+	constructor(id, title,  descirption, image,  year, genre, rating  ) {
+		this.id = id;
+        this.title = title;
+        this.descirption = descirption;
+		this.image = image;
+		this.year = year;
+        this.genre = genre;
+		this.rating = rating;
+		
+		
+	}
 }
+
+// key = "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJhNmMyYWZhZDIwMGY4NDc5N2E2OWIwNGYyZDYwN2I3MCIsIm5iZiI6MTc1ODI5ODYzNy42MDUsInN1YiI6IjY4Y2Q4MjBkNjdiN2IzYzBjZDc0OGRkZCIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.coFHqAS4Zbt2JJhnbbiJ8IqeNHEm7HiILnm9VTzLDq8"
+!(async function () {
+	const options = {
+		method: "GET",
+		headers: {
+			accept: "application/json",
+			Authorization:
+				"Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJhNmMyYWZhZDIwMGY4NDc5N2E2OWIwNGYyZDYwN2I3MCIsIm5iZiI6MTc1ODI5ODYzNy42MDUsInN1YiI6IjY4Y2Q4MjBkNjdiN2IzYzBjZDc0OGRkZCIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.coFHqAS4Zbt2JJhnbbiJ8IqeNHEm7HiILnm9VTzLDq8",
+		},
+	};
+
+	let data = await fetch(
+		"https://api.themoviedb.org/3/movie/popular?language=en-US&page=1",
+		options
+	)
+		.then((response) => response.json())
+		.then((result) => {
+			return result;
+		})
+		.catch((err) => console.error(err));
+
+	console.log(data);
+	console.log(data.results[0].title);
+
+	const Genre = {
+		method: "GET",
+		headers: {
+			accept: "application/json",
+			Authorization:
+				"Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJhNmMyYWZhZDIwMGY4NDc5N2E2OWIwNGYyZDYwN2I3MCIsIm5iZiI6MTc1ODI5ODYzNy42MDUsInN1YiI6IjY4Y2Q4MjBkNjdiN2IzYzBjZDc0OGRkZCIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.coFHqAS4Zbt2JJhnbbiJ8IqeNHEm7HiILnm9VTzLDq8",
+		},
+	};
+
+	let genredata = await fetch(
+		"https://api.themoviedb.org/3/genre/movie/list?language=en",
+		Genre
+	)
+		.then((response) => response.json())
+		.then((result) => {
+			return result;
+		})
+		.catch((err) => console.error(err));
+
+	console.log(genredata);
+
+	let movielist = [];
+
+	for (let i = 0; i < data.results.length; i++) {
+		let image = `https://image.tmdb.org/t/p/w500${data.results[i].poster_path}`;
+		let id = data.results[i].id;
+		let title = data.results[i].title;
+		let descirption = data.results[i].overview;
+		let year = data.results[i].release_date;
+		let genreid = data.results[i].genre_ids[0];
+		let genrename = "";
+		let rating = data.results[i].vote_average;
+
+		for (let k = 0; k < genredata.genres.length; k++) {
+			if (genreid == genredata.genres[k].id) {
+				genrename = genredata.genres[k].name;
+				break;
+			}
+		}
+
+		movielist.push(
+			new Movies(image, year, title, rating, genrename, descirption, id)
+		);
+	}
+
+	console.log("all movies:", movielist);
+
+	movielist.forEach((Movie) => {
+		const containerid = `movieCards_${Movie.genre}`;
+		const container = document.getElementById(containerid);
+		let watchlist = JSON.parse(localStorage.getItem("watchlist")) || [];
+		const isInWatchlist = watchlist.some((item) => item.id === Movie.id);
+
+		if (container) {
+			const card = document.createElement("div");
+			card.className = "card";
+			card.innerHTML = `
+      <img src="${Movie.image}" class="card-img-top" alt="Image Not Found">
+      <h5 class="card-title">${Movie.title}</h5>
+      <p class="card-text">Rating: ${Movie.rating}</p>
+      <a href="#" class="btn-primary" ${isInWatchlist ? "disabled" : ""}>
+        ${isInWatchlist ? "Added" : "Add to Watchlist"}
+      </a>
+    `;
+
+			card.addEventListener("click", (e) => {
+				if (!e.target.classList.contains("btn-primary")) {
+					window.location.href = `(d)individualMoviePage.html?id=${Movie.id}`;
+				} else {
+					e.preventDefault();
+					addToWatchlist(Movie);
+					e.target.textContent = "Added";
+					e.target.disabled = true;
+				}
+			});
+
+			container.appendChild(card);
+		}
+	});
+})();
+
+
+const urlParams = new URLSearchParams(window.location.search);
+const movieId = urlParams.get("id");
+
+async function getMovieDetails() {
+	const options2 = {
+		method: "GET",
+		headers: {
+			accept: "application/json",
+			Authorization:
+				"Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJhNmMyYWZhZDIwMGY4NDc5N2E2OWIwNGYyZDYwN2I3MCIsIm5iZiI6MTc1ODI5ODYzNy42MDUsInN1YiI6IjY4Y2Q4MjBkNjdiN2IzYzBjZDc0OGRkZCIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.coFHqAS4Zbt2JJhnbbiJ8IqeNHEm7HiILnm9VTzLDq8",
+		},
+	};
+
+	try {
+		const response = await fetch(
+			`https://api.themoviedb.org/3/movie/${movieId}?language=en-US`,
+			options2
+		);
+		const movie = await response.json();
+
+		document.getElementById("movieTitle").textContent = movie.title;
+		document.getElementById(
+			"movieImage"
+		).src = `https://image.tmdb.org/t/p/w500${movie.poster_path}`;
+		document.getElementById(
+			"movieRating"
+		).textContent = `Rating: ${movie.vote_average}`;
+		document.getElementById("movieDescription").textContent = movie.overview;
+		document.getElementById("movieGenre").textContent = movie.genres
+			.map((g) => g.name)
+			.join(", ");
+		document.getElementById("movieYear").textContent = movie.release_date;
+
+		const backdrop = document.querySelector(".movie-backdrop");
+		if (backdrop) {
+			backdrop.style.backgroundImage = `url(https://image.tmdb.org/t/p/original${movie.backdrop_path})`;
+		}
+
+	
+	} catch (err) {
+		console.error(err);
+	}
 }
 
-!async function (){
-    
-//private apiKey = '57a4ac9f152331a4dc1321b92927f9cd';
-//private url = 'https://api.themoviedb.org/3';
+getMovieDetails();
 
-const options = {
-   method: 'GET',
-  headers: {
-    accept: 'application/json',
-    Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI1N2E0YWM5ZjE1MjMzMWE0ZGMxMzIxYjkyOTI3ZjljZCIsIm5iZiI6MTc1ODI3OTgyNy44MjUsInN1YiI6IjY4Y2QzODkzMDI1YWYxZDVhODM4MmQ5ZCIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.VjBEwBMEmFCcsUVsBNlFQoLenZHthZDG2KoeT8rrf9g'
-  }
-};
+function addToWatchlist(movie) {
+	let watchlist = JSON.parse(localStorage.getItem("watchlist")) || [];
 
-let data = await fetch('https://api.themoviedb.org/3/account/22322360/watchlist/movies?language=en-US&page=1&sort_by=created_at.asc', options)
-  .then(res => res.json())
-  .then(res => console.log(res))
-  .catch(err => console.error(err));
+	const exists = watchlist.some((item) => item.id === movie.id);
+	if (!exists) {
+		watchlist.push(movie);
+		localStorage.setItem("watchlist", JSON.stringify(watchlist));
+	} else {
+		alert(`${movie.title} is already in your watchlist.`);
+	}
+	console.log("Current Watchlist:", watchlist);
+}
 
-  console.log (data);
+//Watchlist
 
+let watchlist = JSON.parse(localStorage.getItem("watchlist")) || [];
+const container = document.getElementById("watchlistContainer");
 
-    let = movieList=[]
+console.log("Current Watchlist:", watchlist);
 
-    for (i = 0; i < data.movies.length; i++){ 
+if (watchlist.length === 0) {
+	container.innerHTML = "<p>Movie Name .</p>";
+} else {
+	watchlist.forEach((Movie) => {
+		const card = document.createElement("div");
+		card.className = "card";
+		card.innerHTML = `
+    <img src="${Movie.image}" class="card-img-top" alt="Image Not Found">
+    <h5 class="card-title">${Movie.title}</h5>
+    <p class="card-text">Rating: ${Movie.rating}</p>
+    <a href="#" class="btn-primary">Remove From Watchlist</a>
+  `;
 
-        let image = data.Movie[i].video;
-        let Title = data.Movie[i].title;
-        let release_date= data.Movie[i].release_date;
-        let original_language = data.Movie[i].original_language;
-        let overview = data.Movie[i].overview;
-        let rating = data.Movie[i].vote_average;
-        }
-}()
+		card.addEventListener("click", (e) => {
+			if (!e.target.classList.contains("btn-primary")) {
+				window.location.href = `(d)individualMoviePage.html?id=${Movie.id}`;
+			} else {
+				e.preventDefault();
+
+				let updatedWatchlist =
+					JSON.parse(localStorage.getItem("watchlist")) || [];
+				updatedWatchlist = updatedWatchlist.filter(
+					(item) => item.id !== Movie.id
+				);
+				localStorage.setItem("watchlist", JSON.stringify(updatedWatchlist));
+
+				card.remove();
+
+				if (updatedWatchlist.length === 0) {
+					container.innerHTML = "<p>Your watchlist is empty.</p>";
+				}
+			}
+		});
+
+		container.appendChild(card);
+	});
+}
